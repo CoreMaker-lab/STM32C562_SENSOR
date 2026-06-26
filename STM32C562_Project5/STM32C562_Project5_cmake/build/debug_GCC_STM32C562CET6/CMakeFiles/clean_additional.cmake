99a10de0fac8e2ed0c3ed6b9d936b4d8b05bc1a0
Hank Xiao <a845656974@outlook.com>
2026-06-26 11:36:16 +0800

    update

---
+++ STM32C562_Project5/STM32C562_Project5_cmake/build/debug_GCC_STM32C562CET6/CMakeFiles/clean_additional.cmake
@@ -0,0 +1,9 @@
+# Additional clean files
+cmake_minimum_required(VERSION 3.16)
+
+if("${CONFIG}" STREQUAL "" OR "${CONFIG}" STREQUAL "debug_GCC_STM32C562CET6")
+  file(REMOVE_RECURSE
+  "STM32C562_Project5.elf"
+  "STM32C562_Project5.map"
+  )
+endif()
---
+++ STM32C562_Project5/STM32C562_Project5_cmake/generated/hal/mx_gpio_default.c
@@ -0,0 +1,38 @@
+/**
+  ******************************************************************************
+  * @file           : mx_gpio_default.c
+  * @brief          : gpio_default Peripheral initialization
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2026 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the mx_stm32c5xx_hal_drivers_license.md file
+  * in the same directory as the generated code.
+  * If no mx_stm32c5xx_hal_drivers_license.md file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+
+/* Includes ------------------------------------------------------------------*/
+#include "mx_gpio_default.h"
+
+/* Exported types ------------------------------------------------------------*/
+/* Exported constants --------------------------------------------------------*/
+/* Private define ------------------------------------------------------------*/
+/* Private functions ---------------------------------------------------------*/
+/* Exported variables by reference -------------------------------------------*/
+
+/******************************************************************************/
+/* Exported functions for GPIO in HAL layer                                   */
+/******************************************************************************/
+system_status_t mx_gpio_default_init(void)
+{
+  return SYSTEM_OK;
+}
+
+system_status_t mx_gpio_default_deinit(void)
+{
+  return SYSTEM_OK;
+}
